7a3b6179f5b0adfebfbc7d37f5209d8d0071d3b5
muhammed-aslam-pm <muhammedaslampm707@gmail.com>
2023-11-29 19:40:54 +0530

    colors added

--- lib/utils/color_constants.dart
+++ lib/utils/color_constants.dart
@@ -1,1 +1,9 @@
-class ColorConstants {}
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static Color primaryColor = Color(0xff586f6b);
+  static Color primaryBackgroundColor = Color(0xffddd5d0);
+  static Color secondaryColor1 = Color(0xffcfc0bd);
+  static Color secondaryColor2 = Color(0xffb8b8aa);
+  static Color secondaryColor3 = Color(0xff7f9183);
+}
