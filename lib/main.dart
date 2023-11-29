ef78c3ba36c6f41df4381635fcd0f1f5176b493c
muhammed-aslam-pm <muhammedaslampm707@gmail.com>
2023-11-29 19:36:30 +0530

    home screen & clor constants created

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_project_1/view/home_screen/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
